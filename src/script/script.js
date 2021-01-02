3f7dd000e8e1416bd1fc087e663d0ef2ac6f3652
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-01-02 16:32:22 -0300

    feat: create cactus, dino jumping

---
+++ src/script/script.js
@@ -0,0 +1,48 @@
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+
+let isJumping = false;
+
+function handleKeyUp(e) {
+  if (e.keyCode === 32) {
+    if (!isJumping) {
+      jump();
+    }
+  }
+}
+
+function jump() {
+  let position = 0;
+
+  isJumping = true;
+  let upInterval = setInterval(() => {
+    if (position >= 150){
+      clearInterval(upInterval);
+      let downInterval = setInterval(() => {
+        if (position <= 0) {
+          clearInterval(downInterval);
+          isJumping = false;
+        } else {
+          position -= 20;
+          dino.style.bottom = position + 'px';
+        }
+      }, 20)
+    } else {
+      position += 20;
+      dino.style.bottom = position + 'px';
+    }
+    
+  }, 20)
+};
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosistion = 1000;
+
+  cactus.classList.add('cactus');
+  cactus.styles.left = 1000 + 'px';
+  background.appendChild(cactus);
+}
+
+createCactus;
+document.addEventListener('keyup', handleKeyUp)
