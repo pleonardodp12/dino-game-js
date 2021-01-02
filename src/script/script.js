184f9649b9bd3042d50d1d364065afc034f3aeef
pleonardodp12 <pauloleonardo_dp@outlook.com>
2021-01-02 16:57:00 -0300

    feat: colision effect and game over h1

--- src/script/script.js
+++ src/script/script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(e) {
   if (e.keyCode === 32) {
@@ -12,8 +13,6 @@ function handleKeyUp(e) {
 }
 
 function jump() {
-  let position = 0;
-
   isJumping = true;
   let upInterval = setInterval(() => {
     if (position >= 150){
@@ -37,12 +36,29 @@ function jump() {
 
 function createCactus() {
   const cactus = document.createElement('div');
-  let cactusPosistion = 1000;
+  let cactusPosition = 1000;
+  let randomTime = Math.random() * 6000;
 
   cactus.classList.add('cactus');
-  cactus.styles.left = 1000 + 'px';
+  cactus.style.left = 1000 + 'px';
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      clearInterval(leftInterval)
+      document.body.innerHTML = '<h1 class="game-over">Game over</h1>'
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = cactusPosition + 'px';
+    }
+  }, 20);
+
+  setTimeout(createCactus, randomTime)
 }
 
-createCactus;
-document.addEventListener('keyup', handleKeyUp)
+createCactus();
+document.addEventListener('keypress', handleKeyUp)
